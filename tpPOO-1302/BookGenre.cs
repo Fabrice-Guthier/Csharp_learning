cb6189a9d12031d713c224e8a0dcb742b6d87242
Guthier Fabrice <fabrice.guthier@gmail.com>
2025-02-13 16:04:55 +0100

    Add files via upload

---
+++ tpPOO-1302/BookGenre.cs
@@ -0,0 +1,16 @@
+﻿namespace tpPOO_1302
+{
+
+    public enum BookGenre
+    {
+        scifi,
+        manga,
+        novel,
+        comic,
+        documentary,
+        fantasy,
+        youth,
+        thriller,
+        adventure
+    }
+}
