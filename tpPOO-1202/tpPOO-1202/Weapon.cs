24616af3c14368995c7cfe8c549e2b37853b38dd
Guthier Fabrice <fabrice.guthier@gmail.com>
2025-02-13 10:32:09 +0100

    Add files via upload

---
+++ tpPOO-1202/tpPOO-1202/Weapon.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpPOO_1202
+{
+    public class Weapon
+    {
+        private string weaponName;
+        private int attackPoints;
+
+        public Weapon()
+        {
+            weaponName = "";
+            attackPoints = 0;
+        }
+
+        public Weapon(string initWeaponName, int initAttackPoints)
+        {
+            weaponName = initWeaponName;
+            attackPoints = initAttackPoints;
+        }
+
+        public void SetWeaponName(string newWeaponName)
+        {
+            weaponName = newWeaponName;
+        }
+        public string GetWeaponName()
+        {
+            return weaponName;
+        }
+
+        public void SetAttackPoints(int newAttackPoints)
+        {
+            attackPoints = newAttackPoints;
+        }
+        public int GetAttackPoints()
+        {
+            return attackPoints;
+        }
+        
+    }
+}
