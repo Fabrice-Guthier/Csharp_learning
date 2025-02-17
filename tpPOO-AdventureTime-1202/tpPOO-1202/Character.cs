3efeffb672a3de943b43df9e91530c73df66f549
Guthier Fabrice <fabrice.guthier@gmail.com>
2025-02-17 20:23:15 +0100

    Add files via upload

---
+++ tpPOO-AdventureTime-1202/tpPOO-1202/Character.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpPOO_1202
+{
+    public class Character
+    {
+        private string name;
+        private int healthPoints;
+        private Weapon hasWeapon;
+
+
+
+        public Character()
+        {
+            name = "";
+            healthPoints = 100;
+            hasWeapon = new Weapon();
+        }
+
+        public Character(string initName, int initHealthPoints)
+        {
+            name = initName;
+            healthPoints = initHealthPoints;
+        }
+        public Character(string initName, int initHealthPoints, Weapon initHasWeapon)
+        {
+            name = initName;
+            healthPoints = initHealthPoints;
+            hasWeapon = initHasWeapon;
+        }
+
+        public void SetName(string newName)
+        {
+            name = newName;
+        }
+        public string GetName()
+        {
+            return name;
+        }
+
+        public void SetHealthPoints(int newHealthPoints)
+        {
+            healthPoints = newHealthPoints;
+        }
+        public int GetHealthPoints()
+        {
+            return healthPoints;
+        }
+
+        public Weapon GetWeapon()
+        {
+            return hasWeapon;
+        }
+
+        public void SetWeapon(Weapon newWeapon)
+        {
+            hasWeapon = newWeapon;
+        }
+
+        public void TriggerTrap(int damageTaken)
+        {
+            healthPoints -= damageTaken;
+        }
+
+        public void AttackWall(Wall wall)
+        {
+            if (hasWeapon.GetAttackPoints() > wall.GetResistance())
+            {
+                wall.SetIsDestroyed(true);
+            }
+        }
+
+        public void PlayerVersusPlayer(Character otherPlayer)
+        {
+            int otherPlayerHealthPoints = otherPlayer.GetHealthPoints();
+            int weaponAttack = hasWeapon.GetAttackPoints();
+            int newOtherHealthPoints = otherPlayerHealthPoints - weaponAttack;
+
+            otherPlayer.SetHealthPoints(newOtherHealthPoints);
+        }
+
+    }
+}
---
+++ tpPOO-AdventureTime-1202/tpPOO-1202/Wall.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpPOO_1202
+{
+    public class Wall
+    {
+        private int resistance;
+        private bool isDestroyed;
+
+        public Wall()
+        {
+            resistance = 0;
+            isDestroyed = false;
+        }
+
+        public Wall(int initResistance, bool initIsDestroyed)
+        {
+            resistance = initResistance;
+            isDestroyed = initIsDestroyed;
+        }
+
+        public void SetResistance(int newResistance)
+        {
+            resistance = newResistance;
+        }
+        public int GetResistance()
+        {
+            return resistance;
+        }
+        public void SetIsDestroyed(bool newIsDestroyed)
+        {
+            if (resistance <= 0)
+            {
+                isDestroyed = true;
+            }
+        }
+        public bool GetIsDestroyed()
+        {
+            return isDestroyed;
+        }
+    }
+}
